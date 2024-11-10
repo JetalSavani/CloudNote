2b9616ec7217fe66b317334b889e2edad4f21356
JetalSavani <102848084+JetalSavani@users.noreply.github.com>
2024-11-11 00:37:55 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -10,24 +10,10 @@ const port = 4000 || 5000;
 app.use(cors());    //you can not reverse api direct from browser this why we need to use core package in express
 app.use(express.json());
 
-
-app.get('/', (req, res) => {
-    res.send('hello world')
-  })
 //Avilable routes
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/notes', require('./routes/notes'));
 
-// app.use('/test', require('./routes/notes'));
-// app.get('/test', (req, res) => {
-//     // Handle user creation
-//     console.log("request user",req );
-//   });
-
-
-
 app.listen(port, () => {
     console.log(`CloudNote Backend listening at http://localhost:${port}`)
 })
-
-
